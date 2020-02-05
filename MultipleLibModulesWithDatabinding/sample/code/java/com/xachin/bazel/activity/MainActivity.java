eb469c918fac65408a53f6025ee3f98591eb8458
Sachin Tewari <xachin@gmail.com>
2020-02-05 12:43:51 +0800

    Copy LibModuleWithDatabinding to MultipleLibModulesWithDatabinding

---
+++ MultipleLibModulesWithDatabinding/sample/code/java/com/xachin/bazel/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.xachin.bazel.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.sample_main);
+    }
+
+}
