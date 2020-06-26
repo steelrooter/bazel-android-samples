4d56e05756ef39f3be2d6dd309534f1220960570
Sachin Tewari <xachin@gmail.com>
2020-06-26 09:13:14 +0530

    Add flag for Desugar strategy

--- hello-world/src/main/java/com/xachin/playground/hello_world/MainActivity.java
+++ hello-world/src/main/java/com/xachin/playground/hello_world/MainActivity.java
@@ -3,7 +3,6 @@ package com.xachin.playground.hello_world;
 import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.view.View;
 
 import com.xachin.playground.clock.ClockViewModel;
 import com.xachin.playground.clock.ClockViewModelImpl;
@@ -20,15 +19,5 @@ public class MainActivity extends Activity {
         ActivityMainBinding binding =
             DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setVm(clockViewModel);
-
-        /*
-        //FIXME: XML listener not working with Bazel. Ugly Workaround!
-        binding.change.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                clockViewModel.changeTimeFormat();
-            }
-        });
-         */
     }
 }
